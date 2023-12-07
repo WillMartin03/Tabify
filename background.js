7e3c5a106d04ef260004cfa5607e24beff163a2c
William <willmartin2048@gmail.com>
2023-12-06 19:08:17 -0500

    Cross Script Communication + Patch

--- background.js
+++ background.js
@@ -1,11 +1,68 @@
 var tabArr = [];
 
-function checkForDuplicate(tab)
+// LISTENER FOR COMMUNICATION WITH `popup.js`
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+	if (request.action === 'updateCache') {
+		// Update setting within cache
+		checkSetting.cache[request.setting] = request.value;
+	} else if (request.action === 'checkAllDuplicates') {
+		tabArr.forEach(function (tab) {
+			// Check for duplicate tabs in tabArr
+			checkForDuplicate(tab, true);
+		});
+	}
+});
+
+function initializeExtension()
 {
+	// Add existing tabs to tabArr
+	chrome.tabs.query({}, function (existingTabs) {
+		existingTabs.forEach(function (tab) {
+			tabArr.push(tab);
+			console.log("Add tab", tab.id);
+		});
+	});
+
+	// Check for duplicate tabs in tabArr
+	tabArr.forEach(function (tab) {
+		checkForDuplicate(tab);
+	});
+
+	console.log("Extension Loaded.\ntabArr: ", tabArr);
+
+	return;
+}
+
+function checkForDuplicate(tab, preventSwitch=false)
+{
+	/*
+		Extensions can't remove NTP (New Tab Page) tabs.
+		Unchecked runtime.lastError: Cannot remove NTP tab.
+	*/
+	if (tab.url.includes("://newtab"))
+		return;
+
+	/*
+		! Currently experiencing an issue where bing searches create a new tab,
+		! and on that new tab load the same url THEN the next page, which leads
+		! to the tab being seen as a duplicate during the process and deleted
+		TODO: Make a real solution for this
+	*/
+	if (tab.url.includes("search?"))
+		return;
+
+	/*
+		boolean isDuplicate = if any tab url matches current tab url
+		boolean pendingDuplicate = if any tab url matches current tab pendingUrl
+	*/
 	const isDuplicate = tabArr.some(t => t.url === tab.url && t !== tab);
-	if (isDuplicate && !tab.url.includes("://newtab") && !tab.url.includes("search?")) {
+	const pendingDuplicate = tab.pendingUrl !== undefined ? tabArr.some(t => t.url === tab.pendingUrl && t !== tab) : false;
+
+	if (isDuplicate || pendingDuplicate) {
 		chrome.tabs.remove(tab.id, function () {
 			console.log("Closed duplicate tab:", tab.id, tab);
+			if (preventSwitch)
+				return;
 			const originalTab = tabArr.find(t => t.url === tab.url && t !== tab);
 			if (originalTab) {
 				chrome.tabs.update(originalTab.id, { active: true }, function(updatedTab) {
@@ -23,7 +80,6 @@ function getSetting(setting, callback) {
 			console.error(chrome.runtime.lastError);
 			callback(false);
 		} else {
-			// Check if the setting exists in the storage
 			if (setting in result) {
 				// If the setting is found, return its boolean value
 				callback(!!result[setting]);
@@ -37,15 +93,17 @@ function getSetting(setting, callback) {
 
 // Function to check setting, either from cache or by fetching from storage
 function checkSetting(setting, callback) {
+	// Initialize the cache
+	checkSetting.cache = checkSetting.cache || {};
+
 	// Check if the setting is already cached
-	if (checkSetting.cache && setting in checkSetting.cache) {
+	if (setting in checkSetting.cache) {
 		callback(checkSetting.cache[setting]);
 	} else {
 		// If not cached, fetch from storage using getSetting
 		getSetting(setting, function(value) {
 
 			// Cache the value
-			checkSetting.cache = checkSetting.cache || {};
 			checkSetting.cache[setting] = value;
 
 			// Return the value
@@ -61,36 +119,27 @@ chrome.runtime.onInstalled.addListener((reason) => {
 			url: "popup.html"
 		});
 	}
+	initializeExtension();
 });
 
 chrome.runtime.onStartup.addListener(() => {
-	// Add existing tabs to tabArr
-	chrome.tabs.query({}, function (existingTabs) {
-		existingTabs.forEach(function (tab) {
-			tabArr.push(tab);
-		});
-	});
-
-	// Check for duplicate tabs in tabArr
-	tabArr.forEach(function (tab) {
-		checkForDuplicate(tab);
-	});
-
-	console.log("Extension Loaded.\ntabArr: ", tabArr);
+	initializeExtension();
 });
 
 // CREATE TAB LISTENER
 chrome.tabs.onCreated.addListener(function (newTab) {
 	tabArr.push(newTab);
+	console.log("New tab", newTab.id);
 });
 
 // UPDATE TAB LISTENER
 chrome.tabs.onUpdated.addListener(function (tabID, changeInfo, updatedTab) {
-	getSetting('extensionEnabled', function(isEnabled) {
-		console.log('Extension enabled ->', isEnabled);
+	checkSetting('extensionEnabled', function(isEnabled) {
 
-		// Ignore favIcon changes
-		if (changeInfo.favIconUrl || changeInfo.title)
+		// Ignore favIcon, title changes, and loading tabs
+		if (changeInfo.favIconUrl
+			|| changeInfo.title
+			|| (changeInfo.status && changeInfo.status === "loading"))
 			return;
 
 		console.log("changeInfo:", changeInfo);
@@ -113,6 +162,7 @@ chrome.tabs.onRemoved.addListener(function (tabID) {
 		// Remove the tab from the array
 		if (index !== -1) {
 			tabArr.splice(index, 1);
+			console.log("Removed tab", tabID);
 		}
 	}
 	catch (error) {
--- popup.js
+++ popup.js
@@ -1,3 +1,21 @@
+function updateCache(setting, newValue)
+{
+	// Send a message to update the cache in background.js
+	chrome.runtime.sendMessage({
+		action: 'updateCache',
+		setting: setting,
+		value: newValue
+	});
+}
+
+function checkAllDuplicates()
+{
+	// Send a message to update the cache in background.js
+	chrome.runtime.sendMessage({
+		action: 'checkAllDuplicates'
+	});
+}
+
 document.addEventListener('DOMContentLoaded', function () {
 	// Load the saved settings when the popup is opened
 	chrome.storage.sync.get(['strip_question_mark'], function (result) {
@@ -5,22 +23,27 @@ document.addEventListener('DOMContentLoaded', function () {
 			document.getElementById('stripCheckbox').checked = result.strip_question_mark;
 		}
 	});
-	document.getElementById('stripCheckbox').addEventListener('change', function () {
-		const newSettingValue = this.checked;
-		chrome.storage.sync.set({ 'strip_question_mark': newSettingValue }, function () {
-			console.log('[SETTING]: strip_question_mark ->', newSettingValue);
-		});
-	});
-
 	chrome.storage.sync.get(['extensionEnabled'], function (result) {
 		if (result.extensionEnabled !== undefined) {
 			document.getElementById('extensionEnableCheckbox').checked = result.extensionEnabled;
 		}
 	});
+
+	// Add `change` event handlers
+	document.getElementById('stripCheckbox').addEventListener('change', function () {
+		const newSettingValue = this.checked;
+		chrome.storage.sync.set({ 'strip_question_mark': newSettingValue }, function () {
+			updateCache('strip_question_mark', newSettingValue);
+			console.log('[SETTING]: strip_question_mark ->', newSettingValue);
+		});
+	});
 	document.getElementById('extensionEnableCheckbox').addEventListener('change', function () {
 		const newSettingValue = this.checked;
 		chrome.storage.sync.set({ 'extensionEnabled': newSettingValue }, function () {
+			updateCache('extensionEnabled', newSettingValue);
 			console.log('[SETTING]: extensionEnabled ->', newSettingValue);
+			if (newSettingValue === true)
+				checkAllDuplicates();
 		});
 	});
 });
